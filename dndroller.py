8768b84d10f4f420fa7bf48f6e1473ac855a71a3
kamakazi15 <ricci.mark@gmail.com>
2017-09-17 08:17:43 -0600

    Added random number generators and user input

--- dndroller.py
+++ dndroller.py
@@ -1,2 +1,24 @@
-from random import randint
+import random
 
+# Get user input
+num_d20 = input('Enter number of d20: ')
+num_d12 = input('Enter number of d12: ')
+num_d10 = input('Enter number of d10: ')
+num_d8 = input('Enter number of d8: ')
+num_d6 = input('Enter number of d6: ')
+num_d4 = input('Enter number of d4: ')
+modifier = input('Enter bonus: ')
+
+# Roll the dice
+d20 = random.randint(1,20)
+d12 = random.randint(1,12)
+d10 = random.randint(1,10)
+d8 = random.randint(1,8)
+d6 = random.randint(1,6)
+d4 = random.randint(1,4)
+
+# Figure out the total
+total = (num_d20*d20)+(num_d12*d12)+(num_d10*d10)+(num_d8*d8)+(num_d6*d6)+(num_d4*d4)+modifier
+
+print("You rolled ", num_d20, "d20 + ", num_d12, "d12 + ", num_d10, "d10 + ", num_d8, "d8 + ", num_d6, "d6 + ", num_d4, "d4 + ", modifier)
+print("Your final roll was ", total, ".")
